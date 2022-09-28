9f4f44e7aaa9c92a70d983662566760f3987b781
Maria Eduarda de Araujo Freire <95583989+MariaE-duarda@users.noreply.github.com>
2022-09-28 16:23:29 -0300

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 if(window.ethereum){
     // Quando ele tiver MetaMask
-    document.querySelector('button').addEventListener('click',  async()=>{
+    document.querySelector('.metameskLogin').addEventListener('click',  async()=>{
         try{
         const accounts = await ethereum.request({method:'eth_requestAccounts'});
         account = accounts[0];
