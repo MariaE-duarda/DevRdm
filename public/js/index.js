4c34e2ac89ce9496b3a35fd61c8f402e750e7177
Maria Eduarda de Araujo Freire <95583989+MariaE-duarda@users.noreply.github.com>
2022-10-16 10:43:58 -0300

    update - cadastro

--- public/js/index.js
+++ public/js/index.js
@@ -11,3 +11,30 @@ if(window.ethereum){
 else{
 }
 
+cadast = 0
+function telaCadastro(){
+    if (cadast == 0){
+        cadast = 1
+        document.getElementById('devolta').style.display = 'none';
+        document.getElementById('usuário').style.display = 'block';
+        document.getElementById('textLogin').style.display = 'none';
+        document.getElementById('textCadastro').style.display = 'block';
+        document.getElementById('semConta').style.display = 'none';
+        document.getElementById('temConta').style.display = 'inline-block';
+        document.getElementById('btnCadastro').style.display = 'none';
+        document.getElementById('btnLogin').style.display = 'block';
+
+    } else{
+        cadast = 0
+        document.getElementById('devolta').style.display = 'block';
+        document.getElementById('usuário').style.display = 'none';
+        document.getElementById('textLogin').style.display = 'block';
+        document.getElementById('textCadastro').style.display = 'none';
+        document.getElementById('temConta').style.display = 'none';
+        document.getElementById('semConta').style.display = 'inline-block';
+        document.getElementById('btnCadastro').style.display = 'block';
+        document.getElementById('btnLogin').style.display = 'none';
+
+
+    }
+}
