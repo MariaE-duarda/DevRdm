d05af2650ac6a70ada9d2a21f182cad59b09ee3c
Maria Eduarda de Araujo Freire <95583989+MariaE-duarda@users.noreply.github.com>
2022-10-18 17:01:07 -0300

    finalização - modal compartilhar

--- public/js/telaMenu.js
+++ public/js/telaMenu.js
@@ -90,6 +90,8 @@ function trocarCor(){
     document.getElementById('menuInfos').style.background = '#173a759c';
     document.getElementById('colorInfor').style.color = '#4fa2e6';
     document.getElementById('center').style.backgroundColor = '#2f62b9ec';
+    document.getElementById('center2').style.backgroundColor = '#2f62b9ec';
+
     
   } else{
     trokColor = 0
@@ -106,6 +108,6 @@ function trocarCor(){
     document.getElementById('menuInfos').style.background = '#2512449c';
     document.getElementById('colorInfor').style.color = 'blueviolet';
     document.getElementById('center').style.backgroundColor = '#9268d6ea';
-
+    document.getElementById('center2').style.backgroundColor = '';
   }
 }
